bacd1d421b4b10a700046a5d766a2b93c0c114a8
Miltiade <vero@tutamail.com>
2026-07-01 08:42:12 +0300

    created db config file

---
+++ app/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',        # 'root' pour dev
+    'password': 'RBiFZKCHSJdFttXxEzLS3N8Z',  # mot de passe associé
+    'database': 'crm',
+    'port': 3306,          # port MySQL par défaut
+}
